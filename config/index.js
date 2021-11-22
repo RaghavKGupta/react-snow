1c0b9eb29f3557d4eda54ac3a764be1f8592aeae
Raghav Gupta <rgupta11@terpmail.umd.edu>
2021-11-22 11:04:36 -0500

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
 const dev = process.env.NODE_ENV !== 'production'
 export const username = 'adminraghav'
-export const psd = 'pdipw'
+export const psd = ''
 export const server = dev ? 'http://localhost:3000' : 'https://yourwebsite.com'
