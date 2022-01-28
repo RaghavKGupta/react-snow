c5cb037b08b3550b4df38600e9f914879999144d
Raghav Gupta <rgupta11@terpmail.umd.edu>
2022-01-28 15:49:20 -0500

    cors fix

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,8 @@ export const getServerSideProps = async () => {
   const res = await fetch(`https://dev69061.service-now.com/api/now/table/x_682526_page_bu_0_cwig_page_builder`, {
     method: 'get',
     headers: {
-        'Authorization': 'Basic ' + btoa(`${username}:${psd}`)
+        'Authorization': 'Basic ' + btoa(`${username}:${psd}`),
+        "Access-Control-Allow-Origin": "*"
       }
   })
   const articles = await res.json()
