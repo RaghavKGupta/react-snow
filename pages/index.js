425fc5b19361fcf6218aa61666e329576bd93a02
Raghav Gupta <rgupta11@terpmail.umd.edu>
2022-01-28 14:49:36 -0500

    csr test

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,37 @@
 import { username, psd } from '../config'
 import ArticleList from '../components/ArticleList'
+import { useEffect, useState } from "react";
 
 
 export default function Home({ articles }) {
-  try {
-    return (
-      <div>
-        <ArticleList articles={articles.result} />
+
+  const [data, setData] = useState(null)
+  const [isLoading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+    fetch(`https://dev69061.service-now.com/api/now/table/x_682526_page_bu_0_cwig_page_builder`, {
+      method: 'get',
+      headers: {
+          'Authorization': 'Basic ' + btoa(`${username}:${psd}`)
+        }
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data)
+        setLoading(false)
+      })
+  }, [])
+
+  if (isLoading) return <p>Loading...</p>
+  if (!data) return <p>No data</p>
+
+  return (
+    <div>
+       <div>
+        <ArticleList articles={data.result} />
       </div>
-    );
-  } catch (e) {
-    console.log(e)
-    return null;
-  }
-  
+    </div>
+  )
 }
 
-export const getServerSideProps = async () => {
-  /*const res = await fetch(`${server}/api/articles`)*/
-  const res = await fetch(`https://dev69061.service-now.com/api/now/table/x_682526_page_bu_0_cwig_page_builder`, {
-    method: 'get',
-    headers: {
-        'Authorization': 'Basic ' + btoa(`${username}:${psd}`)
-      }
-  })
-  const articles = await res.json()
-
-  return {
-    props: {
-      articles,
-    }
-  }
-}
